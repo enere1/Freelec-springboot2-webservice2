624dc6491feb3e48a96171e6e0f058c0481d7c3c
Leesungon <enere1@naver.com>
2020-06-04 21:27:55 +0900

    JPA利用、登録、修正、紹介のAPIを作り

--- src/main/java/com/leesungon/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/leesungon/book/springboot/domain/posts/Posts.java
@@ -1,19 +1,17 @@
 package com.leesungon.book.springboot.domain.posts;
 
+import com.leesungon.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,4 +30,10 @@ public class Posts {
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
 }
