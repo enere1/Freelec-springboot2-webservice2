af1a66397d8b4987672cf43d510d8e0c928691d6
Leesungon <enere1@naver.com>
2020-06-06 17:29:48 +0900

    Article 修正、削除画面作り

---
+++ src/main/java/com/leesungon/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,24 @@
+package com.leesungon.book.springboot.web.dto;
+
+import com.leesungon.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String Author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.Author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+
+    }
+
+
+}
