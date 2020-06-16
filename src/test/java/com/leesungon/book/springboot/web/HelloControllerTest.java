874e74e1a5a15978370f7c4d47bc498aa74d2651
Leesungon <enere1@naver.com>
2020-06-16 21:30:08 +0900

    test 成功

--- src/test/java/com/leesungon/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/leesungon/book/springboot/web/HelloControllerTest.java
@@ -1,24 +1,37 @@
-package com.leesungon.book.springboot;
+package com.leesungon.book.springboot.web;
 
+import com.leesungon.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
+
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE
+                        , classes = SecurityConfig.class)
+        })
+
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles="USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -26,4 +39,5 @@ public class HelloControllerTest {
                 .andExpect(status().isOk()) //perform결과 검증,status검증,서버상태 검증,OK는 서버상태 200인지 확인
                 .andExpect(content().string(hello));//결과를 검증,응답 본문 내용 검증, hello값이 controller에서 온것인지 확인
     }
+
 }
