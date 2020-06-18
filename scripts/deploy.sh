e1d1d89e84d7350d8f9b22d8eb87af13312e6d3d
Leesungon <enere1@naver.com>
2020-06-19 00:36:36 +0900

    test2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl Freelec-springboot2-webservice2 | grep jar | awk '{print $1}')
 
-if[ -z "$CURRENT_PID" ]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
