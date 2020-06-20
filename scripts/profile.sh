863a4a1ea8ee0c458480702ddc2b99e7e576d058
Leesungon <enere1@naver.com>
2020-06-20 11:47:02 +0900

    CD TEST4

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# 쉬고 있는 profile 찾기 : real1이 사용 중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
+
 function find_idle_profile()
 {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "/%{http_code}" http://localhost/profile)
-   if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+   if [ ${RESPONSE_CODE} -ge 400 ]
     then
         CURRENT_PROFILE=real2
     else
@@ -20,7 +20,6 @@ function find_idle_profile()
     echo "${IDLE_PROFILE}"
 }
 
-# 쉬고 있는 profile의 port 찾기
 function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
